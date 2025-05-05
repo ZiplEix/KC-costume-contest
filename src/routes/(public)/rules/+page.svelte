1914c13456f33c7057253a2827335568719b5b83
ZiplEix <ZiplEix@users.noreply.github.com>
2025-05-06 01:29:03 +0900

    Adds rules page skeleton
    Creates a new rules page with a basic structure and placeholder text.
    Includes a commented-out section for potential future content.

---
+++ src/routes/(public)/rules/+page.svelte
@@ -0,0 +1,10 @@
+<main class="p-4 flex flex-col gap-y-4 mb-16">
+    <h2 class="text-2xl">Rules</h2>
+
+    <!-- <p class="text-lg">
+        1. Each participant can submit one costume.
+    </p> -->
+    <p class="text-lg">
+        Not redacted yet
+    </p>
+</main>
